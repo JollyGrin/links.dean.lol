3c6c044fc4f3078cc0056f21c935c74025f3bdd0
JollyGrin <jollygrin@protonmail.com>
2020-08-08 20:55:24 +0200

    update

---
+++ docs/_next/static/yrEIpePikApIwPL9nvlDJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-f1ee750a29d4d61aa139.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a98d98d178927e175cb7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
